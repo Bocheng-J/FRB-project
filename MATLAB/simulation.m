d0b11fef56e2c492fce85d01f08465ffe7ee6b15
xiaoc1108 <60213862+xiaoc1108@users.noreply.github.com>
2021-01-28 15:24:24 +0100

    Add files via upload

--- MATLAB/simulation.m
+++ MATLAB/simulation.m
@@ -10,10 +10,10 @@ dt=tscale/N;                    % time interval of each sampling point
 t=0:dt:(N-1)*dt;        
 f=100;                          % frequency of Vs1 in Hz
 phase=0;                        % phase of Vs1
-A=1;                            % amplitude of Vs1
+A=10;                            % amplitude of Vs1
 Vs1 = A*sin(2*pi*f*t+phase);
-x1 = max(Vs1)./sqrt(2);         % root mean square of Vs1
-Vs1 = Vs1./x1;                  % normalize Vs1
+% x1 = max(Vs1)./sqrt(2);         % root mean square of Vs1
+% Vs1 = Vs1./x1;                  % normalize Vs0
 
 % plot
 figure(1);
@@ -29,23 +29,29 @@ ylabel('amplitude/V');
 % parameter
 G1 = 10;                        % voltage gain of A1 in dB
 Ph1 = 0;                      % phase introduced by A1
-Vn1 = 40;                        % noise introduced by A1 in dB
-Vs1_Power = sum(abs(Vs1).^2);
-Vs1_Power = 20*log10(Vs1_Power);
-SNR = Vs1_Power-Vn1;
+Vn1 = 1;                        % noise introduced by A1 in dB
+
+sigPower = sum(Vs1.^2)/N;
+sigPower = 10*log10(sigPower);
+noisePower = 10^(Vn1/10);
+noise = sqrt(noisePower)*randn(1,N);
+A1_Out = Vs1+noise;
+
+
+figure(2);
+plot(t.*1000, A1_Out); title('signal + noise');
 
 % convert dB to linear
 G1_Lin = 10^(G1/20);
-
+% 
 % apply to Vs1
-% A1_Out = awgn(Vs1,Vn1);
-% A1_Out = A1_Out.* G1_Lin;
-
-A1_Out = Vs1.*G1_Lin;
-A1_Out = awgn(A1_Out,SNR);
-
+A1_Out = A1_Out.* G1_Lin;
+% 
+% A1_Out = Vs1.*G1_Lin;
+% A1_Out = awgn(A1_Out,SNR);
+% 
 % plot
-figure(2);
+figure(3);
 plot(t.*1000,A1_Out);            % times 1000 to change the unit from s to ms
 str_title2 = sprintf('Vs1 after amplifier A1, with G = %d dB, Ph1 = %d dB and Vn1 = %d dB', G1, Ph1, Vn1);
 title(str_title2);
@@ -53,7 +59,10 @@ title(str_title2);
 xlabel('t/ms');
 ylabel('amplitude/V');
 
-
+% figure(4);
+% Vs1 = A*sin(2*pi*f*t+phase);
+% Vs1 = awgn(Vs1,16, 'measured', 'db');
+% plot(t.*1000, Vs1);
 
 
 
