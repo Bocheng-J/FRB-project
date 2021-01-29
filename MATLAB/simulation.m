d6a324eeac115f0f4e27f48fe90285a60ef41724
Bocheng-J <60817764+Bocheng-J@users.noreply.github.com>
2021-01-29 16:02:48 +0100

    add phase shift and FFT

--- MATLAB/simulation.m
+++ MATLAB/simulation.m
@@ -22,33 +22,53 @@ title(str_title1);
 %axis([-inf,+inf,-1,+1]);
 xlabel('t/ms');
 ylabel('amplitude/V');
+hold on;
 
 
 %% Amplifier A1
 % parameter
 G1 = 10;                        % voltage gain of A1 in dB
-Ph1 = 0;                      % phase introduced by A1
+Ph1 = 45;                      % phase introduced by A1
 Vn1 = 1;                        % noise introduced by A1 in dB
+
+Ph1 = Ph1*pi/180; 
+Vs1 = A*sin(2*pi*f*t+Ph1);
 
 sigPower_Lin = sum(Vs1.^2)/N;       % signal power in watt
 sigPower = 10*log10(sigPower_Lin);  % signal power in dB
 noisePower_Lin = 10^(Vn1/10);           % noise power in watt
 noise = sqrt(noisePower_Lin)*randn(1,N);    % generate noise signal
+
 A1_Out = Vs1+noise;                 % add noise to signal
-
-
 
 G1_Lin = 10^(G1/20); % convert voltage gain from dB to linear
 
 A1_Out = A1_Out.* G1_Lin;           % Output of amplifier A1
 
 % plot
-figure(2);
+%figure(2);
 plot(t.*1000,A1_Out);            % times 1000 to change the unit from s to ms
-str_title2 = sprintf('Vs1 after amplifier A1, with G = %d dB, Ph1 = %d and Vn1 = %d dB', G1, Ph1, Vn1);
-title(str_title2);
-xlabel('t/ms');
-ylabel('amplitude/V');
+% str_title2 = sprintf('Vs1 after amplifier A1, with G = %d dB, Ph1 = %d and Vn1 = %d dB', G1, Ph1, Vn1);
+% title(str_title2);
+% xlabel('t/ms');
+% ylabel('amplitude/V');
+
+
+
+%% FFT
+y = fft(A1_Out);
+fs = 1000;                               % sample frequency 100Mhz
+F_range = (0:N-1)*(fs/N);
+power = abs(y).^2/N;
+
+
+figure(3)
+plot(F_range,power);
+xlabel('Frequency');
+ylabel('Power');
+
+
+
 
 
 
