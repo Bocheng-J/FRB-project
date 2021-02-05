eaf4c284e3ec3ab2a6ffbd6e220ce119ac8a758e
xiaoc1108 <chenxiao961108@gmail.com>
2021-02-05 14:15:55 +0100

    delete unused files

--- MATLAB/ExampleCode/github_repo/README.md
+++
@@ -1,2 +0,0 @@
-# Pulse-dispersion-simulation
-Femtosecond pulse dispersion simulation tool for students, researchers and other users.
--- MATLAB/test_bandpass.m
+++
@@ -1,9 +0,0 @@
-fs = 4000;
-t = 0:1/fs:1-1/fs;
-y = sin(2*pi*200*t);
-figure;
-plot(y);
-
-bandpassY = bandpass(y,[100 300],fs);
-figure;
-plot(bandpassY);
